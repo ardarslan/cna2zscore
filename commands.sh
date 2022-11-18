3fd656a2b0a5b4c6bca95c8640a810a8482aa1fe
Arda Arslan <aarslan@ethz.ch>
2022-11-18 23:37:01 +0100

    Don't use batch norm since batch size is 4, use dropout instead

--- commands.sh
+++ commands.sh
@@ -8,9 +8,12 @@ for CANCER_TYPE in blca all
             do
             for NORMALIZE_INPUT_OUTPUT in false true
                 do
-                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 1 --use_residual_connection false
-                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection false
-                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection true
+                for DROPOUT in 0.0 0.5
+                    do
+                    bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 1 --use_residual_connection false --dropout DROPOUT
+                    bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection false --dropout DROPOUT
+                    bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection true --dropout DROPOUT
+                    done
                 done
             done
         done
