7593bb117affb1e1287e56e3ecd30474b866f4f3
Arda Arslan <aarslan@ethz.ch>
2022-11-18 18:28:09 +0100

    Updated commands

--- commands.sh
+++ commands.sh
@@ -8,9 +8,9 @@ for CANCER_TYPE in blca all
             do
             for NORMALIZE_INPUT_OUTPUT in false true
                 do
-                bsub -n 2 -W 24:00 -R "rusage[mem=16384, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 1 --use_residual_connection false
-                bsub -n 2 -W 24:00 -R "rusage[mem=16384, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection false
-                bsub -n 2 -W 24:00 -R "rusage[mem=16384, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection true
+                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 1 --use_residual_connection false
+                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection false
+                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_input $NORMALIZE_INPUT_OUTPUT --normalize_output $NORMALIZE_INPUT_OUTPUT --num_hidden_layers 2 --use_residual_connection true
                 done
             done
         done
