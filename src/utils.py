c7ed732a48dd33dc2da42011f1d8902099bb3610
Arda Arslan <aarslan@ethz.ch>
2022-11-18 20:50:46 +0100

    Fixed bug

--- src/utils.py
+++ src/utils.py
@@ -240,8 +240,8 @@ def save_test_results(cfg: Dict[str, Any], test_results_dict: Dict[str, Any], en
     all_ground_truths_1d = all_ground_truths.ravel()
     all_predictions_1d = all_predictions.ravel()
 
-    bottom_left = np.maximum(np.minimum(all_ground_truths_1d), np.minimum(all_predictions_1d))
-    top_right = np.minimum(np.maximum(all_ground_truths_1d), np.maximum(all_predictions_1d))
+    bottom_left = np.maximum(np.min(all_ground_truths_1d), np.min(all_predictions_1d))
+    top_right = np.minimum(np.min(all_ground_truths_1d), np.max(all_predictions_1d))
 
     plt.figure(figsize=(12, 12))
     plt.title(f"Correlation: {np.round(all_corr, 2)}, P-value: {np.round(all_p_value, 2)}")
