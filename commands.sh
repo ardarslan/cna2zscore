51b1f33643f4132a6d62994b5b30d08cc878e241
Arda Arslan <aarslan@ethz.ch>
2022-11-21 00:45:32 +0100

    Updated commands

--- commands.sh
+++ commands.sh
@@ -1,17 +1,24 @@
 cd src
 
-for HIDDEN_DIMENSION in 1000 2500 5000 max
-    do
-    for CANCER_TYPE in blca all
-        do
-        for DATASET in rppa2gex cnapurity2gex avggexsubtype2gex
-            do
-            for NORMALIZE_OUTPUT in false true
-                do
-                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers 0 --use_residual_connection false
-                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers 1 --use_residual_connection false
-                bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers 2 --use_residual_connection true
-                done
-            done
-        done
-    done
+# for HIDDEN_DIMENSION in 1000 2500 5000 max
+#     do
+#     for CANCER_TYPE in blca all
+#         do
+#         for DATASET in rppa2gex cnapurity2gex avggexsubtype2gex
+#             do
+#             for NORMALIZE_OUTPUT in false true
+#                 do
+#                 bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers 0 --use_residual_connection false
+#                 bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers 1 --use_residual_connection false
+#                 bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --hidden_dimension $HIDDEN_DIMENSION --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers 2 --use_residual_connection true
+#                 done
+#             done
+#         done
+#     done
+
+bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type blca --dataset cnapurity2gex --hidden_dimension 1000 --normalize_output true --num_hidden_layers 1 --use_residual_connection false
+bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type blca --dataset rppa2gex --hidden_dimension 2500 --normalize_output true --num_hidden_layers 1 --use_residual_connection false
+
+bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type all --dataset cnapurity2gex --hidden_dimension 5000 --normalize_output true --num_hidden_layers 1 --use_residual_connection false
+bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type all --dataset rppa2gex --hidden_dimension 1000 --normalize_output true --num_hidden_layers 0 --use_residual_connection false
+bsub -n 2 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python main.py --cancer_type all --dataset avggexsubtype2gex --hidden_dimension 1000 --normalize_output true --num_hidden_layers 0 --use_residual_connection false
