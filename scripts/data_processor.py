9335a6adf8333a95a8b1dd5d4903b4efb2890d9f
Arda Arslan <aarslan@ethz.ch>
2022-12-02 19:15:56 +0100

    Updated data processing

--- scripts/data_processor.py
+++ scripts/data_processor.py
@@ -134,14 +134,14 @@ print("Processed CNA data.")
 print("Processing Cancer Type data...")
 
 cancer_type_file_name = "TCGA_phenotype_denseDataOnlyDownload.tsv"
-cancer_type_full_name_to_abbrreviation_mapping_file_name = "cancer_type_full_name_to_abbrreviation_mapping.tsv"
+cancer_type_full_name_to_abbreviation_mapping_file_name = "cancer_type_full_name_to_abbrreviation_mapping.tsv"
 
-cancer_type_full_name_to_abbrreviation_mapping = dict(pd.read_csv(os.path.join(data_dir, raw_folder_name, cancer_type_full_name_to_abbrreviation_mapping_file_name), sep="\t").values)
+cancer_type_full_name_to_abbreviation_mapping = dict(pd.read_csv(os.path.join(data_dir, raw_folder_name, cancer_type_full_name_to_abbreviation_mapping_file_name), sep="\t").values)
 
 cancer_type_df = pd.read_csv(os.path.join(data_dir, raw_folder_name, cancer_type_file_name), sep="\t")
 cancer_type_df = cancer_type_df.rename(columns={"sample": "sample_id", "_primary_disease": "cancer_type"})
 cancer_type_df = cancer_type_df[["sample_id", "cancer_type"]]
-cancer_type_df["cancer_type"] = cancer_type_df["cancer_type"].swifter.apply(lambda x: cancer_type_full_name_to_abbrreviation_mapping[x])
+cancer_type_df["cancer_type"] = cancer_type_df["cancer_type"].swifter.apply(lambda x: cancer_type_full_name_to_abbreviation_mapping[x])
 
 cancer_type_one_hot_df = pd.get_dummies(data=cancer_type_df, columns=["cancer_type"])
 
