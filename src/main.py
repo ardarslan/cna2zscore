bd48a6baec8578a18a522a67333a60a119cf22bb
Arda Arslan <aarslan@ethz.ch>
2022-12-02 14:31:50 +0100

    Fixed dump

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 
 from train import train
@@ -57,8 +59,7 @@ if __name__ == "__main__":
     model = load_model(cfg=cfg, dataset=dataset, logger=logger)
     test_results_dict = test(cfg=cfg, data_loaders=data_loaders, model=model, loss_function=val_test_loss_function, dataset=dataset, logger=logger)
 
-    import pickle
-    with open("test_results_dict.pickle", "wb") as writer:
-        writer.write(test_results_dict, writer)
+    with open("test_results_dict.pickle", "wb") as file:
+        pickle.dump(obj=test_results_dict, file=file)
 
     # save_test_results(cfg=cfg, test_results_dict=test_results_dict, entrezgene_ids=dataset.entrezgene_ids, logger=logger)
