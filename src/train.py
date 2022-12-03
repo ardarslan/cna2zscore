d113d8339e04cd53ab08394574aec8e2aba445b4
Arda Arslan <aarslan@ethz.ch>
2022-12-03 23:39:18 +0100

    Disable gradient accummulation

--- src/train.py
+++ src/train.py
@@ -7,16 +7,16 @@ from torch.utils.data import Dataset, DataLoader
 def train(cfg: Dict[str, Any], data_loaders: Dict[str, DataLoader], model: nn.Module, loss_function, dataset: Dataset, optimizer) -> None:
     model.train()
 
-    effective_batch_size = cfg["effective_batch_size"]
+    # effective_batch_size = cfg["effective_batch_size"]
     train_data_loader = data_loaders["train"]
-    num_train_samples = len(dataset.train_idx)
-    num_batches = len(train_data_loader)
-    observed_sample_count = 0
+    # num_train_samples = len(dataset.train_idx)
+    # num_batches = len(train_data_loader)
+    # observed_sample_count = 0
 
-    for batch_idx, batch in enumerate(train_data_loader):
+    for _, batch in enumerate(train_data_loader):
         X = batch["X"]
         y = batch["y"]
-        observed_sample_count += X.shape[0]
+        # observed_sample_count += X.shape[0]
 
         if cfg["use_batch_normalization"] and X.shape[0] == 1:
             continue
@@ -27,6 +27,8 @@ def train(cfg: Dict[str, Any], data_loaders: Dict[str, DataLoader], model: nn.Mo
         if cfg["normalize_output"]:
             y = (y - dataset.y_train_mean) / dataset.y_train_std
 
+        optimizer.zero_grad()
+
         yhat = model(X)
         loss = loss_function(yhat, y)
 
@@ -35,11 +37,13 @@ def train(cfg: Dict[str, Any], data_loaders: Dict[str, DataLoader], model: nn.Mo
         if cfg["l2_reg_coeff"] > 0:
             loss += cfg["l2_reg_coeff"] * sum(p.pow(2.0).sum() for p in model.parameters())
 
-        if ((batch_idx + 1) < num_batches) or (num_train_samples % effective_batch_size == 0):
-            (loss / float(effective_batch_size)).backward()
-        else:
-            (loss / float(num_train_samples % effective_batch_size)).backward()
+        # if ((batch_idx + 1) < num_batches) or (num_train_samples % effective_batch_size == 0):
+        #     (loss / float(effective_batch_size)).backward()
+        # else:
+        #     (loss / float(num_train_samples % effective_batch_size)).backward()
 
-        if (observed_sample_count % effective_batch_size == 0) or observed_sample_count == num_train_samples:
-            optimizer.step()
-            optimizer.zero_grad()
+        # if (observed_sample_count % effective_batch_size == 0) or observed_sample_count == num_train_samples:
+        #     optimizer.step()
+        #     optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
