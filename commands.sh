ae38728e941709745808f36d4c2d9932692f4e62
Arda Arslan <aarslan@ethz.ch>
2022-12-04 01:05:57 +0100

    Updated commands

--- commands.sh
+++ commands.sh
@@ -2,7 +2,7 @@ cd src
 
 
 for DATASET in 'unthresholdedcnapurity2gex' 'thresholdedcnapurity2gex' 'rppa2gex'; do
-    for NORMALIZE_OUTPUT in false true; do
+    for NORMALIZE_OUTPUT in true; do
         for HIDDEN_DIMENSION in 2500 5000 10000; do
             for NUM_HIDDEN_LAYERS in 0 1 2; do
                 for CANCER_TYPE in 'blca' 'all'; do
@@ -11,7 +11,7 @@ for DATASET in 'unthresholdedcnapurity2gex' 'thresholdedcnapurity2gex' 'rppa2gex
                     bsub -n 4 -W 24:00 -R "rusage[mem=16378, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers $NUM_HIDDEN_LAYERS --hidden_dimension $HIDDEN_DIMENSION --dropout 0.0 --l1_reg_coeff 0.0 --l2_reg_coeff 0.0
 
                     # Dropout
-                    for DROPOUT in 0.25 0.5; do
+                    for DROPOUT in 0.5; do
                         bsub -n 4 -W 24:00 -R "rusage[mem=16378, ngpus_excl_p=1]" python main.py --cancer_type $CANCER_TYPE --dataset $DATASET --normalize_output $NORMALIZE_OUTPUT --num_hidden_layers $NUM_HIDDEN_LAYERS --hidden_dimension $HIDDEN_DIMENSION --dropout $DROPOUT --l1_reg_coeff 0.0 --l2_reg_coeff 0.0
                     done
 
