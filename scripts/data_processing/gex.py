c55c756d906b7743d8337fd81ce56027b3862368
Arda Arslan <aarslan@ethz.ch>
2022-12-12 12:07:15 +0100

    Try on euler

--- scripts/data_processing/gex.py
+++ scripts/data_processing/gex.py
@@ -26,9 +26,17 @@ def process_gex_data(data_dir: str, raw_folder_name: str, processed_folder_name:
 
     gex_df = gex_df.dropna(axis=0)
 
+    gex_df = gex_df[["entrezgene_id"] + [column for column in gex_df.columns if column.split("-")[-1] in tumor_sample_ids]]
+
+    def select_gene_with_max_total_expression(x):
+        gene_total_expressions = x.drop(columns=["entrezgene_id"]).values.sum(axis=1).ravel()
+        max_expression_index = np.argmax(gene_total_expressions)
+        return x.iloc[max_expression_index, :]
+
+    gex_df = gex_df.groupby("entrezgene_id").apply(lambda x: select_gene_with_max_total_expression(x)).reset_index(drop=False)
+
     gex_df.set_index("entrezgene_id", inplace=True)
 
-    gex_df = gex_df[[column for column in gex_df.columns if column.split("-")[-1] in tumor_sample_ids]]
     gex_df = gex_df.T
     gex_df = gex_df.reset_index(drop=False)
 
