c294eff9d929537d27c9fe6e879dd2091d7eac7b
Arda Arslan <aarslan@ethz.ch>
2022-12-13 17:43:47 +0100

    Updated l1 l2

--- commands.sh
+++ commands.sh
@@ -26,12 +26,12 @@ for DATASET in 'unthresholdedcna2gex' 'unthresholdedcnapurity2gex' 'thresholdedc
                         sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_coeff 0.0 --l2_reg_coeff 0.0"
 
                         # L1 regularization
-                        for L1_REG_COEFF in 0.001 0.01; do
+                        for L1_REG_COEFF in 0.001 0.01 0.1; do
                             sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_coeff $L1_REG_COEFF --l2_reg_coeff 0.0"
                         done
 
                         # L2 regularization
-                        for L2_REG_COEFF in 0.001 0.01; do
+                        for L2_REG_COEFF in 0.001 0.01 0.1; do
                             sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_coeff 0.0 --l2_reg_coeff $L2_REG_COEFF"
                         done
                     done
