204940596de487cab9094ffe676526456ea760e2
Arda Arslan <aarslan@ethz.ch>
2022-12-31 17:59:16 +0100

    Try only l1

--- commands.sh
+++ commands.sh
@@ -9,28 +9,28 @@ for DATASET in 'unthresholdedcnapurity2gex' 'thresholdedcnapurity2gex' 'rppa2gex
                     declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
                     declare -a HIDDEN_DIMENSION_OPTIONS=(0)
                     declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(0)
-                    declare -a L1_REG_DIAGONAL_COEFF_OPTIONS=(0.0 0.001 0.01 0.1)
-                    declare -a L2_REG_DIAGONAL_COEFF_OPTIONS=(0.0 0.001 0.01 0.1)
-                    declare -a L1_REG_NONDIAGONAL_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
-                    declare -a L2_REG_NONDIAGONAL_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
+                    declare -a L1_REG_DIAGONAL_COEFF_OPTIONS=(0.001) # (0.0 0.001 0.01 0.1)
+                    declare -a L2_REG_DIAGONAL_COEFF_OPTIONS=(0.001) # (0.0 0.001 0.01 0.1)
+                    declare -a L1_REG_NONDIAGONAL_COEFF_OPTIONS=(0.01) # (0.001 0.01 0.1 1.0)
+                    declare -a L2_REG_NONDIAGONAL_COEFF_OPTIONS=(0.01) # (0.001 0.01 0.1 1.0)
                 elif [[ $MODEL = 'mlp' ]]; then
                     declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
-                    declare -a HIDDEN_DIMENSION_OPTIONS=(2500 5000 10000)
-                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
-                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
-                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
+                    declare -a HIDDEN_DIMENSION_OPTIONS=(5000) # (2500 5000 10000)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(2) # (1 2 3)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.001) # (0.001 0.01 0.1 1.0)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.001) # (0.001 0.01 0.1 1.0)
                 elif [[ $MODEL = 'mlp_per_chromosome_all' || $MODEL = 'mlp_per_chromosome_24']]; then
                     declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
                     declare -a HIDDEN_DIMENSION_OPTIONS=('max')
-                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
-                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
-                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1) # (1 2 3)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.001) # (0.001 0.01 0.1 1.0)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.001) # (0.001 0.01 0.1 1.0)
                 elif [[ $MODEL = 'rescon_mlp' ]]; then
-                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false true)
-                    declare -a HIDDEN_DIMENSION_OPTIONS=(2500 5000 10000)
-                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
-                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
-                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1 1.0)
+                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false) # (false true)
+                    declare -a HIDDEN_DIMENSION_OPTIONS=(5000) # (2500 5000 10000)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(2) # (1 2 3)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.01) # (0.001 0.01 0.1 1.0)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.01) # (0.001 0.01 0.1 1.0)
                 else
                     echo "MODEL is not a valid $MODEL."
                     exit 1
@@ -41,7 +41,7 @@ for DATASET in 'unthresholdedcnapurity2gex' 'thresholdedcnapurity2gex' 'rppa2gex
                         for HIDDEN_DIMENSION in "${HIDDEN_DIMENSION_OPTIONS}"; do
 
                             # No regularization
-                            sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff 0.0 --l2_reg_nondiagonal_coeff 0.0 --dropout 0.0"
+                            # sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff 0.0 --l2_reg_nondiagonal_coeff 0.0 --dropout 0.0"
 
                             # L1 regularization
                             if [[ $MODEL = "linear" || $MODEL = "linear_per_chromosome_all" || $MODEL = "linear_per_chromosome_24" ]]; then
@@ -61,26 +61,26 @@ for DATASET in 'unthresholdedcnapurity2gex' 'thresholdedcnapurity2gex' 'rppa2gex
                             fi
 
                             # L2 regularization
-                            if [[ $MODEL = "linear" || $MODEL = "linear_per_chromosome_all" || $MODEL = "linear_per_chromosome_24" ]]; then
-                                for L2_REG_DIAGONAL_COEFF in "${L2_REG_DIAGONAL_COEFF_OPTIONS}"; do
-                                    for L2_REG_NONDIAGONAL_COEFF in "${L2_REG_NONDIAGONAL_COEFF_OPTIONS}"; do
-                                        if [[ $L2_REG_NONDIAGONAL_COEFF -lt $L2_REG_DIAGONAL_COEFF ]]; then
-                                            continue
-                                        else
-                                            sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff $L2_REG_DIAGONAL_COEFF --l2_reg_nondiagonal_coeff $L2_REG_NONDIAGONAL_COEFF --dropout 0.0"
-                                        fi
-                                    done
-                                done
-                            else
-                                for L2_REG_COEFF in "${L2_REG_COEFF_OPTIONS}"; do
-                                    sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff $L2_REG_COEFF --l2_reg_nondiagonal_coeff $L2_REG_COEFF --dropout 0.0"
-                                done
-                            fi
+                            # if [[ $MODEL = "linear" || $MODEL = "linear_per_chromosome_all" || $MODEL = "linear_per_chromosome_24" ]]; then
+                            #     for L2_REG_DIAGONAL_COEFF in "${L2_REG_DIAGONAL_COEFF_OPTIONS}"; do
+                            #         for L2_REG_NONDIAGONAL_COEFF in "${L2_REG_NONDIAGONAL_COEFF_OPTIONS}"; do
+                            #             if [[ $L2_REG_NONDIAGONAL_COEFF -lt $L2_REG_DIAGONAL_COEFF ]]; then
+                            #                 continue
+                            #             else
+                            #                 sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff $L2_REG_DIAGONAL_COEFF --l2_reg_nondiagonal_coeff $L2_REG_NONDIAGONAL_COEFF --dropout 0.0"
+                            #             fi
+                            #         done
+                            #     done
+                            # else
+                            #     for L2_REG_COEFF in "${L2_REG_COEFF_OPTIONS}"; do
+                            #         sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff $L2_REG_COEFF --l2_reg_nondiagonal_coeff $L2_REG_COEFF --dropout 0.0"
+                            #     done
+                            # fi
 
                             # Dropout
-                            for DROPOUT in 0.25; do
-                                sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff 0.0 --l2_reg_nondiagonal_coeff 0.0 --dropout $DROPOUT"
-                            done
+                            # for DROPOUT in 0.25; do
+                            #     sbatch --time=1440 --ntasks=2 --mem-per-cpu=16384 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff 0.0 --l2_reg_nondiagonal_coeff 0.0 --dropout $DROPOUT"
+                            # done
 
                         done
                     done
