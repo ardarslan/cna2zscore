4a873033172b450edbe1de170d533a4100a65289
Arda Arslan <aarslan@ethz.ch>
2023-01-03 19:38:51 +0100

    Try on euler

--- commands.sh
+++ commands.sh
@@ -1,51 +1,49 @@
 cd src
 
-for MODEL in 'linear' 'linear_per_chromosome_24' 'mlp_per_chromosome_24' 'mlp' 'transformer' 'linear_per_chromosome_all' 'mlp_per_chromosome_all' 'rescon_mlp'; do
+for CANCER_TYPE in 'blca' 'all';
     for DATASET in 'unthresholdedcnapurity2gex' 'rppa2gex'; do
-        for CANCER_TYPE in 'blca' 'all'; do
-            if [[ $MODEL = 'linear' || $MODEL = 'linear_per_chromosome_all' || $MODEL = 'linear_per_chromosome_24' ]]; then
-                declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
-                declare -a HIDDEN_DIMENSION_OPTIONS=(0)
-                declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(0)
-                declare -a L1_REG_DIAGONAL_COEFF_OPTIONS=(0.0 0.001 0.01 0.1)
-                declare -a L2_REG_DIAGONAL_COEFF_OPTIONS=(0.0 0.001 0.01 0.1)
-                declare -a L1_REG_NONDIAGONAL_COEFF_OPTIONS=(0.001 0.01 0.1)
-                declare -a L2_REG_NONDIAGONAL_COEFF_OPTIONS=(0.001 0.01 0.1)
-            elif [[ $MODEL = 'mlp' ]]; then
-                declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
-                declare -a HIDDEN_DIMENSION_OPTIONS=(2500 5000 10000)
-                declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
-                declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-                declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-            elif [[ $MODEL = 'mlp_per_chromosome_all' || $MODEL = 'mlp_per_chromosome_24' ]]; then
-                declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
-                declare -a HIDDEN_DIMENSION_OPTIONS=(max)
-                declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
-                declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-                declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-            elif [[ $MODEL = 'rescon_mlp' ]]; then
-                declare -a RESCON_DIAGONAL_W_OPTIONS=(false true)
-                declare -a HIDDEN_DIMENSION_OPTIONS=(2500 5000 10000)
-                declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
-                declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-                declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-            elif [[ $MODEL = 'transformer' ]]; then
-                declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
-                declare -a HIDDEN_DIMENSION_OPTIONS=(0)
-                declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(0)
-                declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-                declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
-            else
-                echo "MODEL is not a valid $MODEL."
-                exit 1
-            fi
 
-            if [[ $DATASET = 'rppa2gex' ]]; then
-                declare -a GENE_TYPE_OPTIONS=("rppa_genes")
-            else
-                declare -a GENE_TYPE_OPTIONS=("1000_highly_expressed_genes" "5000_highly_expressed_genes" "rppa_genes")
+        if [[ $DATASET = 'rppa2gex' ]]; then
+            declare -a GENE_TYPE_OPTIONS=("rppa_genes")
+        else
+            declare -a GENE_TYPE_OPTIONS=("1000_highly_expressed_genes" "5000_highly_expressed_genes" "rppa_genes")
 
-            for GENE_TYPE in "${GENE_TYPE_OPTIONS[@]}"; do
+        for GENE_TYPE in "${GENE_TYPE_OPTIONS[@]}"; do
+            for MODEL in 'linear' 'linear_per_chromosome_24' 'mlp_per_chromosome_24' 'mlp' 'transformer' 'linear_per_chromosome_all' 'mlp_per_chromosome_all' 'rescon_mlp'; do
+                if [[ $MODEL = 'linear' || $MODEL = 'linear_per_chromosome_all' || $MODEL = 'linear_per_chromosome_24' ]]; then
+                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
+                    declare -a HIDDEN_DIMENSION_OPTIONS=(0)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(0)
+                    declare -a L1_REG_DIAGONAL_COEFF_OPTIONS=(0.0 0.001 0.01 0.1)
+                    declare -a L2_REG_DIAGONAL_COEFF_OPTIONS=(0.0 0.001 0.01 0.1)
+                    declare -a L1_REG_NONDIAGONAL_COEFF_OPTIONS=(0.001 0.01 0.1)
+                    declare -a L2_REG_NONDIAGONAL_COEFF_OPTIONS=(0.001 0.01 0.1)
+                elif [[ $MODEL = 'mlp' ]]; then
+                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                elif [[ $MODEL = 'mlp_per_chromosome_all' || $MODEL = 'mlp_per_chromosome_24' ]]; then
+                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                elif [[ $MODEL = 'rescon_mlp' ]]; then
+                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false true)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(1 2 3)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                elif [[ $MODEL = 'transformer' ]]; then
+                    declare -a RESCON_DIAGONAL_W_OPTIONS=(false)
+                    declare -a NUM_NONLINEAR_LAYERS_OPTIONS=(0)
+                    declare -a L1_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                    declare -a L2_REG_COEFF_OPTIONS=(0.001 0.01 0.1)
+                else
+                    echo "MODEL is not a valid $MODEL."
+                    exit 1
+                fi
+
+
                 for RESCON_DIAGONAL_W in "${RESCON_DIAGONAL_W_OPTIONS[@]}"; do
                     for NUM_NONLINEAR_LAYERS in "${NUM_NONLINEAR_LAYERS_OPTIONS[@]}"; do
                         for HIDDEN_DIMENSION in "${HIDDEN_DIMENSION_OPTIONS[@]}"; do
