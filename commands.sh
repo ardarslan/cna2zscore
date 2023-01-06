910076f01bf8075c09d6bee5813eeebbdddac250
Arda Arslan <aarslan@ethz.ch>
2023-01-06 20:51:38 +0100

    New hyperparameters

--- commands.sh
+++ commands.sh
@@ -28,42 +28,42 @@ for CANCER_TYPE in 'all'; do
                 RESCON_DIAGONAL_W_OPTIONS=(false)
                 HIDDEN_DIMENSION_OPTIONS=(0.0)
                 NUM_NONLINEAR_LAYERS_OPTIONS=(0)
-                L1_REG_DIAGONAL_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L2_REG_DIAGONAL_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L1_REG_NONDIAGONAL_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L2_REG_NONDIAGONAL_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
+                L1_REG_DIAGONAL_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L2_REG_DIAGONAL_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L1_REG_NONDIAGONAL_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L2_REG_NONDIAGONAL_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
                 GENE_EMBEDDING_SIZE_OPTIONS=(0)
                 NUM_ATTENTION_HEADS_OPTIONS=(0)
             elif [[ $MODEL = 'mlp' ]]; then
                 RESCON_DIAGONAL_W_OPTIONS=(false)
                 HIDDEN_DIMENSION_OPTIONS=(0.10 0.25 0.5 1.0)
                 NUM_NONLINEAR_LAYERS_OPTIONS=(1 2)
-                L1_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L2_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
+                L1_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L2_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
                 GENE_EMBEDDING_SIZE_OPTIONS=(0)
                 NUM_ATTENTION_HEADS_OPTIONS=(0)
             elif [[ $MODEL = 'mlp_per_chromosome_all' || $MODEL = 'mlp_per_chromosome_24' ]]; then
                 RESCON_DIAGONAL_W_OPTIONS=(false)
                 HIDDEN_DIMENSION_OPTIONS=(0.10 0.25 0.5 1.0)
                 NUM_NONLINEAR_LAYERS_OPTIONS=(1 2)
-                L1_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L2_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
+                L1_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L2_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
                 GENE_EMBEDDING_SIZE_OPTIONS=(0)
                 NUM_ATTENTION_HEADS_OPTIONS=(0)
             elif [[ $MODEL = 'rescon_mlp' ]]; then
                 RESCON_DIAGONAL_W_OPTIONS=(false true)
                 HIDDEN_DIMENSION_OPTIONS=(0.10 0.25 0.5 1.0)
                 NUM_NONLINEAR_LAYERS_OPTIONS=(1 2)
-                L1_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L2_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
+                L1_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L2_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
                 GENE_EMBEDDING_SIZE_OPTIONS=(0)
                 NUM_ATTENTION_HEADS_OPTIONS=(0)
             elif [[ $MODEL = 'transformer' ]]; then
                 RESCON_DIAGONAL_W_OPTIONS=(false)
                 HIDDEN_DIMENSION_OPTIONS=(0.0)
                 NUM_NONLINEAR_LAYERS_OPTIONS=(0)
-                L1_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
-                L2_REG_COEFF_OPTIONS=(0.000001 0.00001 0.0001 0.001)
+                L1_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
+                L2_REG_COEFF_OPTIONS=(0.00001 0.0001 0.001 0.01 0.1)
                 GENE_EMBEDDING_SIZE_OPTIONS=(4 16 64)
                 NUM_ATTENTION_HEADS_OPTIONS=(4 8)
             else
@@ -78,7 +78,7 @@ for CANCER_TYPE in 'all'; do
                             for NUM_NONLINEAR_LAYERS in "${NUM_NONLINEAR_LAYERS_OPTIONS[@]}"; do
                                 for HIDDEN_DIMENSION in "${HIDDEN_DIMENSION_OPTIONS[@]}"; do
                                     for DROPOUT in 0.0 0.25 0.33; do
-                                        for LEARNING_RATE in 0.00001 0.0001 0.001; do
+                                        for LEARNING_RATE in 0.000001 0.00001 0.0001; do
 
                                             # No regularization
                                             sbatch --time=1440 --ntasks=2 --mem-per-cpu=32768 --gpus=1 --gres=gpumem:12288 --wrap="python main.py --dataset $DATASET --cancer_type $CANCER_TYPE --gene_type $GENE_TYPE --model $MODEL --rescon_diagonal_W $RESCON_DIAGONAL_W --gene_embedding_size $GENE_EMBEDDING_SIZE --num_attention_heads $NUM_ATTENTION_HEADS --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --hidden_dimension $HIDDEN_DIMENSION --learning_rate $LEARNING_RATE --l1_reg_diagonal_coeff 0.0 --l1_reg_nondiagonal_coeff 0.0 --l2_reg_diagonal_coeff 0.0 --l2_reg_nondiagonal_coeff 0.0 --dropout $DROPOUT"
